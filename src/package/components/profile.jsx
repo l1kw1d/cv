c22c04d35f0852bf449abc2bd5d517f8592e4ce8
L1kw1d <l1kw1d@users.noreply.github.com>
2020-03-30 00:57:08 -0400

    Update profile.jsx

--- src/package/components/profile.jsx
+++ src/package/components/profile.jsx
@@ -41,7 +41,7 @@ const DEFAULT_OPTIONS = Object.freeze({
     locale: 'en',
     customization: {
         imageHeader: {
-            url: 'https://cdn.filestackcontent.com/8I2wVnCRTFxypXRYLRsp',
+            url: 'https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
             alt: 'Default Banner'
         }
     },
